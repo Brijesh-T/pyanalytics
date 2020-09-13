2c4b4153ac5ca14dc5c67cad3cba0ac4cf185e72
Brijesh-T <71176943+Brijesh-T@users.noreply.github.com>
2020-09-13 16:48:48 +0530

    11

---
+++ file2.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Sep 12 18:47:44 2020
+
+@author: brije
+"""
+list =[1,2,3]
---
+++ untitled0.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Sep 12 18:36:19 2020
+
+@author: brije
+"""
+
+type{1,2,3,4}
